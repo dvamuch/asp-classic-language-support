87f8e63ea930f333c7f8cf40f9b30a3bee5b8496
Artem Selifonov <a.selifonov@naukanet.ru>
2026-02-09 18:51:30 +0300

    Работают html-инспекции а asp-теги распознаются как asp-теги

--- src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/AspTokenTypes.kt
+++ src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/AspTokenTypes.kt
@@ -4,10 +4,11 @@ import com.intellij.lang.html.HTMLLanguage
 import com.intellij.psi.templateLanguages.TemplateDataElementType
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.IFileElementType
+import com.intellij.psi.tree.OuterLanguageElementType
 
 object AspTokenTypes {
     val TEMPLATE_DATA: IElementType = IElementType("ASP_TEMPLATE_DATA", HTMLLanguage.INSTANCE)
-    val OUTER: IElementType = IElementType("ASP_OUTER", AspLanguage)
+    val OUTER: IElementType = OuterLanguageElementType("ASP_OUTER", AspLanguage)
     val TEMPLATE_FILE: IFileElementType = TemplateDataElementType(
         "ASP_TEMPLATE_FILE",
         AspLanguage,
