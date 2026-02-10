1e5d215ee328de50f379bbcda2bba118d355e5a8
Dvamuch <davmuch@gmail.com>
2026-02-10 13:46:52 +0300

    feat: added icon

--- src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/AspFileType.kt
+++ src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/AspFileType.kt
@@ -2,6 +2,7 @@ package dvamuch.aspclassiclanguagesupport2.lang
 
 import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.fileTypes.TemplateLanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object AspFileType : LanguageFileType(AspLanguage), TemplateLanguageFileType {
@@ -11,5 +12,5 @@ object AspFileType : LanguageFileType(AspLanguage), TemplateLanguageFileType {
 
     override fun getDefaultExtension(): String = "asp"
 
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon? = IconLoader.getIcon("/icons/asp.svg", AspFileType::class.java)
 }
