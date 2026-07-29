cd0fc80d7755b447df1fe5d34a4c59b276a4e9be
Dvamuch <davmuch@gmail.com>
2026-07-29 16:28:40 +0300

    fix: restore single-file VBScript Find Usages

---
+++ src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/vbscript/VbFindUsagesHandlerFactory.kt
@@ -0,0 +1,50 @@
+package dvamuch.aspclassiclanguagesupport2.lang.vbscript
+
+import com.intellij.find.findUsages.FindUsagesHandler
+import com.intellij.find.findUsages.FindUsagesHandlerFactory
+import com.intellij.find.findUsages.FindUsagesOptions
+import com.intellij.openapi.application.ReadAction
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.usageView.UsageInfo
+import com.intellij.util.Processor
+import dvamuch.aspclassiclanguagesupport2.lang.vbscript.psi.VbDeclarationUtil
+import dvamuch.aspclassiclanguagesupport2.lang.vbscript.psi.VbId
+
+class VbFindUsagesHandlerFactory : FindUsagesHandlerFactory() {
+    override fun canFindUsages(element: PsiElement): Boolean {
+        val id = element as? VbId ?: return false
+        return VbDeclarationUtil.declaration(id) != null
+    }
+
+    override fun createFindUsagesHandler(
+        element: PsiElement,
+        forHighlightUsages: Boolean
+    ): FindUsagesHandler = VbFindUsagesHandler(element)
+}
+
+private class VbFindUsagesHandler(element: PsiElement) : FindUsagesHandler(element) {
+    override fun processElementUsages(
+        element: PsiElement,
+        processor: Processor<in UsageInfo>,
+        options: FindUsagesOptions
+    ): Boolean {
+        val userScope = options.searchScope
+        val fastTrack = options.fastTrack
+        val localScope = ReadAction.compute<LocalSearchScope, RuntimeException> {
+            LocalSearchScope(element.containingFile)
+        }
+        options.searchScope = localScope
+        options.fastTrack = null
+        return try {
+            super.processElementUsages(
+                element,
+                Processor { usage -> processor.process(VbHostUsageInfo.from(usage)) },
+                options
+            )
+        } finally {
+            options.searchScope = userScope
+            options.fastTrack = fastTrack
+        }
+    }
+}
