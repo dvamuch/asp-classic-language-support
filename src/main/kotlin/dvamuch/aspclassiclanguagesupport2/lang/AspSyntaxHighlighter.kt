118632a356722b1019676e5ee2808226844ba7a9
Dvamuch <davmuch@gmail.com>
2026-09-01 18:55:41 +0300

    feat: highlight ASP delimiters like PHP tags

--- src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/AspSyntaxHighlighter.kt
+++ src/main/kotlin/dvamuch/aspclassiclanguagesupport2/lang/AspSyntaxHighlighter.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.tree.IElementType
+import dvamuch.aspclassiclanguagesupport2.lang.vbscript.psi.VbTypes
 
 class AspSyntaxHighlighter : SyntaxHighlighter {
     override fun getHighlightingLexer(): Lexer = AspLexer()
@@ -17,8 +18,15 @@ class AspSyntaxHighlighter : SyntaxHighlighter {
     override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> {
         return when (tokenType) {
             AspTokenTypes.TEMPLATE_DATA -> arrayOf(DefaultLanguageHighlighterColors.TEMPLATE_LANGUAGE_COLOR)
+            VbTypes.ASP_OPEN, VbTypes.ASP_EXPR_OPEN, VbTypes.ASP_CLOSE -> SCRIPTLET_DELIMITER_KEYS
             else -> vbHighlighter.getTokenHighlights(tokenType)
         }
+    }
+
+    private companion object {
+        val SCRIPTLET_DELIMITER_KEYS = arrayOf(
+            TextAttributesKey.find("PHP_TAG") ?: DefaultLanguageHighlighterColors.MARKUP_TAG
+        )
     }
 }
 
